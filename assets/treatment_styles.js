25b5a2125ea13df30eac420caa42cd9791ed6ff5
cpgruber <chasegruber@gmail.com>
2015-10-07 09:14:08 -0400

    initial commit

---
+++ assets/treatment_styles.js
@@ -0,0 +1,137 @@
+var treatmentStyles = {
+    Septic: {
+        name: 'Septic System',
+        icon: 'Septic.png',
+        color: '#00AEEF'
+    },
+    IndividualIA: {
+        name: 'Individual IA',
+        icon: 'IndividualIA.png',
+        color: '#00AEEF'
+    },
+    EnhancedIA: {
+        name: 'Enhanced IA',
+        icon: 'EnhancedIA.png',
+        color: '#00AEEF'
+    },
+    SingleStageCluster: {
+        name: 'Single Stage Cluster',
+        icon: 'Cluster.png',
+        color: '#00AEEF'
+    },
+    TwoStageCluster: {
+        name: 'Two Stage Cluster',
+        icon: 'Cluster.png',
+        color: '#00AEEF'
+    },
+    CentralizedFacilityWithin: {
+        name: 'Centralized Sewer Facility (within watershed)',
+        icon: 'CentralizedFacility.png',
+        color: '#00AEEF'
+    },
+    CentralizedFacilityOutside: {
+        name: 'Centralized Sewer Facility (outside watershed)',
+        icon: 'CentralizedFacility.png',
+        color: '#00AEEF'
+    },
+    SatelliteFacilitySm: {
+        name: 'Satellite Facility (small)',
+        icon: 'SatelliteFacility.png',
+        color: '#00AEEF'
+    },
+    SatelliteFacilityLg: {
+        name: 'Satellite Facility (large)',
+        icon: 'SatelliteFacility.png',
+        color: '#00AEEF'
+    },
+    CompostingToilet: {
+        name: 'Composting Toilets',
+        icon: 'CompostingToilet.png',
+        color: '#00AEEF'
+    },
+    IncineratingToilet: {
+        name: 'Incinerating Toilets',
+        icon: 'CompostingToilet.png',
+        color: '#00AEEF'
+    },
+    PackagingToilet: {
+        name: 'Packaging Toilets',
+        icon: 'CompostingToilet.png',
+        color: '#00AEEF'
+    },
+    UrineDiverting: {
+        name: 'Urine Diverting Toilets',
+        icon: 'UrineDivertingToilet.png',
+        color: '#00AEEF'
+    },
+    FertilizationManagement: {
+        name: 'Fertilization Management',
+        icon: 'FertilizationManagement.png',
+        color: '#FCB016'
+    },
+    StormwaterManagement: {
+        name: 'Stormwater BMP - Non-structural',
+        icon: 'StormwaterBMPPhytobuffers.png',
+        color: '#FCB016'
+    },
+    Aquaculture: {
+        name: 'Aquaculture',
+        icon: 'Aquaculture.png',
+        color: '#14AFB3'
+    },
+    ConstructedWetland: {
+        name: 'Constructed Wetland',
+        icon: 'SaltwaterConstructedWetland.png',
+        color: '#8CC63F'
+    },
+    Phytoirrigation: {
+        name: 'Phytoirrigation',
+        icon: 'Phytoirrigation.png',
+        color: '#00AEEF'
+    },
+    StormwaterBMPPhytobuffers: {
+        name: 'Stormwater BMP - Phytobuffers',
+        icon: 'Phytobuffer.png',
+        color: '#64C298'
+    },
+    Phytoremediation: {
+        name: 'Phytoremediation',
+        icon: 'Phytoremediation.png',
+        color: '#662D90'
+    },
+    PRBInjectionWell: {
+        name: 'Permeable Reactive Barriers',
+        icon: 'PRB.png',
+        color: '#15897A'
+    },
+    FertigationTurf: {
+        name: 'Fertigation Wells - Turf',
+        icon: 'FertigationTurf.png',
+        color: '#14A74B'
+    },
+    FertigationCranBog: {
+        name: 'Fertigation Wells - Cranberry Bog',
+        icon: '',
+        color: '#14A74B'
+    },
+    InletCulvertWidening: {
+        name: 'Inlet/Culvert Wideninig',
+        icon: 'InletCulvertWidening.png',
+        color: '#004B23'
+    },
+    CoastalHabitatRestoration: {
+        name: 'Coastal Habitat Restoration',
+        icon: 'CoastalHabitatRestoration.png',
+        color: '#8CC63F'
+    },
+    FloatingConstructedWetlands: {
+        name: 'Floating Constructed Wetland',
+        icon: 'FloatingConstructedWetland.png',
+        color: '#8CC63F'
+    },
+    SurfaceWaterRemediationWetlands: {
+        name: 'Surface Water Remediation Wetlands',
+        icon: 'SurfaceWaterRemediationWetlands.png',
+        color: '#8CC63F'
+    },
+}
